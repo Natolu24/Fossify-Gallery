9c778344b72fd1090ef69a069c526cbd85549031
Sriram448 <ramthurai11@gmail.com>
2024-05-08 02:13:13 +0200

    first commit

---
+++ Video-speed-control/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        jcenter()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        jcenter()
+        mavenCentral()
+        maven { setUrl("https://jitpack.io") }
+    }
+}
+
+rootProject.name = "Gallery"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include(":app")
+
+// TODO: This will be deprecated in future. Migrate to the newer `pluginManagement { includeBuild() }` mechanism instead of explicitly substituting dependency.
+/*includeBuild("../Commons") {
+    dependencySubstitution {
+        substitute(module("org.fossify:commons")).using(project(":commons"))
+    }
+}*/
